b450f858bea7b0f8c055771134f49a393217a43b
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-05-06 19:16:15 +1000

    Add files via upload

---
+++ Content/Buffs/ChadPetBuff.cs
@@ -0,0 +1,31 @@
+﻿using Bijou.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Bijou.Content.Buffs
+{
+    public class ChadPetBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.vanityPet[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        { // This method gets called every frame your buff is active on your player.
+            player.buffTime[buffIndex] = 18000;
+
+            int projType = ModContent.ProjectileType<PetProj>();
+
+            // If the player is local, and there hasn't been a pet projectile spawned yet - spawn it.
+            if (player.whoAmI == Main.myPlayer && player.ownedProjectileCounts[projType] <= 0)
+            {
+                var entitySource = player.GetSource_Buff(buffIndex);
+
+                Projectile.NewProjectile(entitySource, player.Center, Vector2.Zero, projType, 0, 0f, player.whoAmI);
+            }
+        }
+    }
+}
