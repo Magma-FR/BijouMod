f4fcdcd170546f49b2795d44243b21e742c80879
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-05-06 19:17:59 +1000

    Add files via upload

---
+++ Content/Tiles/BijouOre.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Bijou.Content.Items.Blocks;
+using Microsoft.Xna.Framework;
+using static Terraria.ID.ContentSamples.CreativeHelper;
+using Terraria.ID;
+using Terraria.Audio;
+
+namespace Bijou.Content.Tiles
+{
+    public class BijouOre : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            TileID.Sets.Ore[Type] = true;
+
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileShine[Type] = 900;
+            Main.tileShine2[Type] = true;
+            Main.tileSpelunker[Type] = true;
+            Main.tileOreFinderPriority[Type] = 350;
+
+
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Bijore");
+            AddMapEntry(new Color(21, 170, 235), name);
+
+            DustType = DustID.BlueCrystalShard;
+            ItemDrop = ModContent.ItemType<BijouOreItem>();
+
+            //HitSound = new SoundStyle($"{nameof(RealmOne)}/Assets/Soundss/OldGoldTink");
+
+            HitSound = SoundID.DD2_WitherBeastCrystalImpact;
+            MineResist = 1f;
+            MinPick = 35;
+        }
+    }
+}
---
+++ Content/Tiles/RizzStatuR.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ObjectData;
+using Terraria.GameContent.ObjectInteractions;
+using Terraria.Enums;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Localization;
+using Bijou.Content.Items.Blocks;
+
+namespace Bijou.Content.Tiles
+{
+    public class RizzStatuR : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            // Properties
+            Main.tileSolidTop[Type] = true;
+
+            Main.tileSpelunker[Type] = true;
+            Main.tileShine2[Type] = true;
+            Main.tileShine[Type] = 1200;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+
+
+            MineResist = 10f;
+            MinPick = 30;
+
+            DustType = DustID.IceGolem;
+
+            // Names
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.newTile.Height = 6;
+            TileObjectData.newTile.Width = 5;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16, 16 };
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Bijou Head Monument");
+            AddMapEntry(new Color(30, 200, 200), name);
+
+
+            // Placement
+          
+           
+            //      ChestDrop = ModContent.ItemType<TatteredBarrelItem>();
+
+
+          
+            TileObjectData.addTile(Type);
+        }
+
+        public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) => offsetY = 2;
+
+
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = 1;
+        }
+
+        public override void KillMultiTile(int x, int y, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(x, y), x * 16, y * 16, 48, 32, ModContent.ItemType<RizzStatueItem>(), Main.rand.Next(1, 1));
+
+            if (Main.netMode != NetmodeID.Server)
+            {
+
+                int BGore1 = Mod.Find<ModGore>("HeadGore1").Type;
+                int BGore2 = Mod.Find<ModGore>("HeadGore2").Type;
+
+                var entitySource = new EntitySource_TileBreak(x, y);
+
+                // We don't want Mod.Find<ModGore> to run on servers as it will crash because gores are not loaded on servers
+
+
+                for (int i = 0; i < 1; i++)
+                {
+                    Gore.NewGore(entitySource, new Vector2(x * 16, y * 16), new Vector2(Main.rand.Next(0, 0), Main.rand.Next(0, 0)), BGore1);
+                    Gore.NewGore(entitySource, new Vector2(x * 16, y * 16), new Vector2(Main.rand.Next(0,0), Main.rand.Next(0, 0)), BGore2);
+
+                }
+            }
+
+
+        }
+    }
+}
